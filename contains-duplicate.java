ba34233ded004587ed5695364e83f1048013d8b5
Mohamad Abd El Raouf <48860285+moraouf11@users.noreply.github.com>
2022-09-17 23:32:23 +0200

    Create contains-duplicate.java

---
+++ contains-duplicate.java
@@ -0,0 +1,14 @@
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+           HashMap<Integer,Integer> map=new HashMap<Integer,Integer>();//Creating HashMap    
+
+        for(int i=0; i< nums.length; i++)
+        {
+            if (map.containsKey(nums[i])) return true;
+            else map.put(nums[i],nums[i]);
+        }
+        return false;
+        
+        
+    }
+}

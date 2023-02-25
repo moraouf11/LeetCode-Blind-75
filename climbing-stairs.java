3f959ebdb3bfa325b931a2ec9ff4bd7f2ec45003
Mohamad Abd El Raouf <48860285+moraouf11@users.noreply.github.com>
2023-02-25 17:06:02 +0200

    Create climbing-stairs.java

---
+++ climbing-stairs.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int climbStairs(int n) {
+    if(n <= 0)
+        return 0;
+        if (n==1 || n ==2 || n ==3 )
+            return n ;
+        int[] ways = new int[n];
+        ways[0] = 1;
+        ways[1] =2;
+
+        for(int i=2; i< n ;i++){
+            ways[i] = ways[i-1] + ways[i-2];
+        }
+
+        return ways[n-1] ;
+    }
+}

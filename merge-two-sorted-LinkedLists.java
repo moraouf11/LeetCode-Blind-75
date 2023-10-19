fb3a0d8c92d9afa361124593a849d6711a74054a
Mohamad Radi <48860285+moraouf11@users.noreply.github.com>
2023-10-20 02:24:33 +0300

    Update merge-two-sorted-LinkedLists.java

--- merge-two-sorted-LinkedLists.java
+++ merge-two-sorted-LinkedLists.java
@@ -1,4 +1,9 @@
 /**
+// Problem Statement
+//Input: list1 = [1,2,4], list2 = [1,3,4]
+//Output: [1,1,2,3,4,4]
+ 
+ 
  * Definition for singly-linked list.
  * public class ListNode {
  *     int val;

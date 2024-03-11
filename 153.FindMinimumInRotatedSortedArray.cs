f904081d9225e12869efdea7cbdad53d2c7a2808
Mohamad Radi <48860285+moraouf11@users.noreply.github.com>
2024-03-11 03:21:48 +0200

    Create 153.FindMinimumInRotatedSortedArray.cs

---
+++ 153.FindMinimumInRotatedSortedArray.cs
@@ -0,0 +1,19 @@
+public class Solution {
+    public int FindMin(int[] nums) {
+        int index = 1; 
+            if(nums.Length == 1 || (nums[0] < nums[1] && nums[0] < nums.Last()))
+                return nums[0];        
+        while(index +1 < nums.Length)
+        {                
+            if(nums[index] < nums[index+1] && nums[index] < nums[index-1])
+                return nums[index];
+            if(nums[index] > nums[index+1] && nums[index] > nums[index-1])
+                return nums[index+1];
+
+            if(nums[index] > nums[index-1] && nums[index] < nums[index+1])
+                index +=2;  
+        }
+        return nums[index];
+
+    }
+}

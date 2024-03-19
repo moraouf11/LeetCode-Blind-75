acc7e5f9deb7b634d1f16e35308dd0568a099b23
Mohamad Radi <48860285+moraouf11@users.noreply.github.com>
2024-03-19 03:18:47 +0200

    Create 22.GenerateParentheses.cs

---
+++ 22.GenerateParentheses.cs
@@ -0,0 +1,25 @@
+public class Solution {
+    public IList<string> GenerateParenthesis(int n) 
+    {
+        var results = new List<string>();
+        return backtracking("",results, n, n);
+
+    }
+    IList<string> backtracking(string wellformedString ,List<string> results, int opens, int closes)
+    {
+        if(opens == 0 && closes ==0)
+             results.Add(wellformedString);
+        if(opens == closes)
+        {
+            backtracking(wellformedString + "(", results, opens-1, closes);
+        }
+        else
+        {
+            if(opens > 0 )
+                backtracking(wellformedString + "(", results, opens-1, closes);
+            if(closes > 0 )
+                backtracking(wellformedString + ")", results, opens, closes-1);
+        }
+        return results;
+    }
+}

5da8bbd890c65b6c061fcef3480953c2fb9ad253
Mohamad Radi <48860285+moraouf11@users.noreply.github.com>
2024-05-22 00:08:05 +0300

    Update 235.LowestCommonAncestorOfABinarySearchTree.cs

--- 235.LowestCommonAncestorOfABinarySearchTree.cs
+++ 235.LowestCommonAncestorOfABinarySearchTree.cs
@@ -11,9 +11,9 @@
 
 // According to the definition of LCA on Wikipedia: “The lowest common ancestor is defined between two nodes 
 //     p and q as the lowest node in T that has both p and q as descendants (where we allow a node to be a descendant of itself).”
-// Input: root = [3,5,1,6,2,0,8,null,null,7,4], p = 5, q = 1
-// Output: 3
-// Explanation: The LCA of nodes 5 and 1 is 3.
+// Input: root = [6,2,8,0,4,7,9,null,null,3,5], p = 2, q = 8
+// Output: 6
+// Explanation: The LCA of nodes 2 and 8 is 6.
 
 public class Solution {
     public TreeNode LowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
